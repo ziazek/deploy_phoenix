8e53ad1a2c6c84a8d0a949766583d891dacd3eb0
Zia Zek <ziazek@gmail.com>
2017-06-01 18:16:18 +0800

    Testing hot upgrades

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeployPhoenix.Mixfile do
 
   def project do
     [app: :deploy_phoenix,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
